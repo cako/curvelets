54459ff55fe07575470b2ef21876bedf67372012
cako <c.dacostaf@gmail.com>
2024-02-18 13:54:07 -0800

    reproduce udct library example

--- examples/plot_zone_plate.py
+++ examples/plot_zone_plate.py
@@ -34,7 +34,8 @@ def make_zone_plate(shape: tuple[int, int], amplitude=1, phase=0):
 
 shape = (256, 256)
 zone_plate = make_zone_plate(shape)
-C = UDCT(size=shape)
+cfg = np.array([[3, 3], [6, 6], [12, 6]])
+C = UDCT(size=shape, cfg=cfg)
 
 # %%
 # Uniform Discrete Curvelet Transform Round Trip
@@ -74,3 +75,87 @@ despine(ax)
 ax.set(title=f"Error (max = {opts['vmax']:.2g})")
 
 print(f"Max Error: {opts['vmax']:.2g}")  # noqa: T201
+
+# %%
+# Curvelet Coefficients
+# #####################
+z = coeffs[1][1]
+opts["vmax"] = np.abs(z).max()
+opts["vmin"] = 0
+opts["cmap"] = "gray"
+fig, axs = plt.subplots(1, 2, figsize=(8, 3))
+im = axs[0].imshow(np.abs(z).T, **opts)
+_, cb = create_colorbar(im=im, ax=axs[0])
+fmt = ticker.FuncFormatter(lambda x, pos: f"{x:.0e}")
+cb.ax.yaxis.set_major_formatter(fmt)
+opts["vmax"] = 180
+opts["vmin"] = -opts["vmax"]
+opts["cmap"] = "hsv"
+im = axs[1].imshow(np.angle(z, deg=True).T, **opts)
+_, cb = create_colorbar(im=im, ax=axs[1])
+cb.ax.yaxis.set_major_locator(ticker.MultipleLocator(45))
+cb.ax.yaxis.set_major_formatter(lambda x, pos: f"{x:.0f}°")
+axs[0].set(title="Amplitude")
+axs[1].set(title="Phase")
+fig.suptitle("Scale 1")
+fig.tight_layout()
+
+for i in range(2, max(coeffs.keys())):
+    for j in coeffs[i]:
+        for a in coeffs[i][j]:
+            z = coeffs[i][j][a]
+            opts["vmax"] = np.abs(z).max()
+            opts["vmin"] = 0
+            opts["cmap"] = "gray"
+            fig, axs = plt.subplots(1, 2, figsize=(8, 3))
+            im = axs[0].imshow(np.abs(z).T, **opts)
+            _, cb = create_colorbar(im=im, ax=axs[0])
+            fmt = ticker.FuncFormatter(lambda x, pos: f"{x:.0e}")
+            cb.ax.yaxis.set_major_formatter(fmt)
+            opts["vmax"] = 180
+            opts["vmin"] = -opts["vmax"]
+            opts["cmap"] = "hsv"
+            im = axs[1].imshow(np.angle(z, deg=True).T, **opts)
+            _, cb = create_colorbar(im=im, ax=axs[1])
+            cb.ax.yaxis.set_major_locator(ticker.MultipleLocator(45))
+            cb.ax.yaxis.set_major_formatter(lambda x, pos: f"{x:.0f}°")
+            axs[0].set(title="Amplitude")
+            axs[1].set(title="Phase")
+            fig.suptitle(f"Scale {i} | Direction {j} | Angle {a}")
+            fig.tight_layout()
+
+
+# %%
+# Curvelet Coefficients
+# #####################
+z = coeffs[1][1]
+opts["vmax"] = np.abs(z).max()
+opts["vmin"] = -opts["vmax"]
+opts["cmap"] = "gray"
+fig, axs = plt.subplots(1, 2, figsize=(8, 3))
+for ax, img in zip(axs.ravel(), [z.real, z.imag]):
+    im = ax.imshow(img.T, **opts)
+    _, cb = create_colorbar(im=im, ax=ax)
+    fmt = ticker.FuncFormatter(lambda x, pos: f"{x:.0e}")
+    cb.ax.yaxis.set_major_formatter(fmt)
+axs[0].set(title="Real")
+axs[1].set(title="Imaginary")
+fig.suptitle("Scale 1")
+fig.tight_layout()
+
+for i in range(2, max(coeffs.keys())):
+    for j in coeffs[i]:
+        for a in coeffs[i][j]:
+            z = coeffs[i][j][a]
+            opts["vmax"] = np.abs(z).max()
+            opts["vmin"] = -opts["vmax"]
+            fig, axs = plt.subplots(1, 2, figsize=(8, 3))
+            for ax, img in zip(axs.ravel(), [z.real, z.imag]):
+                im = ax.imshow(img.T, **opts)
+                _, cb = create_colorbar(im=im, ax=ax)
+                fmt = ticker.FuncFormatter(lambda x, pos: f"{x:.0e}")
+                cb.ax.yaxis.set_major_formatter(fmt)
+            axs[0].set(title="Real")
+            axs[1].set(title="Imaginary")
+            fig.suptitle(f"Scale {i} | Direction {j} | Angle {a}")
+            fig.tight_layout()
