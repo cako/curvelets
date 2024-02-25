361b3b7f460702857d04b7958ac491fa93598eed
cako <c.dacostaf@gmail.com>
2024-02-25 00:10:59 -0800

    make typing work for 3.9

--- src/curvelets/numpy/typing.py
+++ src/curvelets/numpy/typing.py
@@ -7,9 +7,15 @@ if sys.version_info >= (3, 10):
 else:
     from typing_extensions import TypeAlias
 
+
 import numpy as np
 import numpy.typing as npt
 
-UDCTCoefficients: TypeAlias = list[list[list[npt.NDArray[np.complexfloating]]]]
+if sys.version_info <= (3, 9):
+    from typing import List  # noqa: UP035
 
-UDCTWindows: TypeAlias = list[list[list[list[npt.NDArray[np.floating]]]]]
+    UDCTCoefficients: TypeAlias = List[List[List[npt.NDArray[np.complexfloating]]]]  # noqa: UP006
+    UDCTWindows: TypeAlias = List[List[List[List[npt.NDArray[np.floating]]]]]  # noqa: UP006
+else:
+    UDCTCoefficients: TypeAlias = list[list[list[npt.NDArray[np.complexfloating]]]]
+    UDCTWindows: TypeAlias = list[list[list[list[npt.NDArray[np.floating]]]]]
