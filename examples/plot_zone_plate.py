d159c48b764398e323f6e00f1ee500eaf169876c
Carlos da Costa <cako@users.noreply.github.com>
2024-11-19 17:46:06 -0800

    add stuff

--- examples/plot_zone_plate.py
+++ examples/plot_zone_plate.py
@@ -108,23 +108,57 @@ for ires, _ in enumerate(coeffs):
             fig.tight_layout()
 
 
+# # %%
+# # Curvelet Coefficients: Real and Imaginary
+# # #########################################
+# opts["cmap"] = "gray"
+# for ires, _ in enumerate(coeffs):
+#     for idir, _ in enumerate(coeffs[ires]):
+#         for iang, _ in enumerate(coeffs[ires][idir]):
+#             z = coeffs[ires][idir][iang]
+#             opts["vmax"] = np.abs(z).max()
+#             opts["vmin"] = -opts["vmax"]
+#             fig, axs = plt.subplots(1, 2, figsize=(8, 3))
+#             for ax, img in zip(axs.ravel(), [z.real, z.imag]):
+#                 im = ax.imshow(img.T, **opts)
+#                 _, cb = create_colorbar(im=im, ax=ax)
+#                 fmt = ticker.FuncFormatter(lambda x, _: f"{x:.0e}")
+#                 cb.ax.yaxis.set_major_formatter(fmt)
+#             axs[0].set(title="Real")
+#             axs[1].set(title="Imaginary")
+#             fig.suptitle(f"Scale {ires} | Direction {idir} | Angle {iang}")
+#             fig.tight_layout()
 # %%
-# Curvelet Coefficients: Real and Imaginary
+# Curvelet Coefficients: Concatenated
 # #########################################
+
+# coeffs_vec = C.vect(coeffs=coeffs)
+# coeffs_img = []
+# for i in range(0, len(coeffs_vec) - zone_plate.size, zone_plate.size):
+#     coeffs_img.append(coeffs_vec[i : i + zone_plate.size].reshape(1, *zone_plate.shape))
+# if len(coeffs_vec) % zone_plate.size != 0:
+#     coeffs_img.append(np.zeros((1, *zone_plate.shape), dtype=coeffs_vec.dtype))
+#     coeffs_img[-1].flat[
+#         len(coeffs_vec) - zone_plate.size : len(coeffs_vec)
+#     ] = coeffs_vec[len(coeffs_vec) - zone_plate.size :]
+# coeffs_img = np.concatenate(coeffs_img, axis=0)
+
+_coeffs_vec = []
+for c in coeffs[:2]:
+    for d in c:
+        for a in d:
+            _coeffs_vec.append(a.ravel())
+coeffs_vec = np.concatenate(_coeffs_vec)
+
+coeffs_img = np.zeros((1, *zone_plate.shape), dtype=coeffs_vec.dtype)
+coeffs_img.flat[: len(coeffs_vec)] = coeffs_vec[: len(coeffs_vec)]
+# %%
+fig, ax = plt.subplots(figsize=(12, 12))
+opts["vmax"] = np.abs(coeffs_vec).max()
+opts["vmin"] = 0
 opts["cmap"] = "gray"
-for ires, _ in enumerate(coeffs):
-    for idir, _ in enumerate(coeffs[ires]):
-        for iang, _ in enumerate(coeffs[ires][idir]):
-            z = coeffs[ires][idir][iang]
-            opts["vmax"] = np.abs(z).max()
-            opts["vmin"] = -opts["vmax"]
-            fig, axs = plt.subplots(1, 2, figsize=(8, 3))
-            for ax, img in zip(axs.ravel(), [z.real, z.imag]):
-                im = ax.imshow(img.T, **opts)
-                _, cb = create_colorbar(im=im, ax=ax)
-                fmt = ticker.FuncFormatter(lambda x, _: f"{x:.0e}")
-                cb.ax.yaxis.set_major_formatter(fmt)
-            axs[0].set(title="Real")
-            axs[1].set(title="Imaginary")
-            fig.suptitle(f"Scale {ires} | Direction {idir} | Angle {iang}")
-            fig.tight_layout()
+opts["aspect"] = "auto"
+im = ax.imshow(np.abs(np.concatenate(coeffs_img, axis=0))[:50, :].T, **opts)
+_, cb = create_colorbar(im=im, ax=ax)
+fmt = ticker.FuncFormatter(lambda x, _: f"{x:.0e}")
+cb.ax.yaxis.set_major_formatter(fmt)
