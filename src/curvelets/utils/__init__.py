659953d05a19c54c30482b7949bdd72525a02bd1
Carlos da Costa <cako@users.noreply.github.com>
2024-11-23 14:09:23 -0800

    feat: lots of new utilities

--- src/curvelets/utils/__init__.py
+++ src/curvelets/utils/__init__.py
@@ -3,8 +3,18 @@ from __future__ import annotations
 __all__ = [
     "apply_along_wedges",
     "array_split_nd",
-    "split_nd",
+    "deepflatten",
     "energy_split",
     "ndargmax",
+    "normal_vector_field",
+    "split_nd",
 ]
-from ._utils import apply_along_wedges, array_split_nd, energy_split, ndargmax, split_nd
+from ._utils import (
+    apply_along_wedges,
+    array_split_nd,
+    deepflatten,
+    energy_split,
+    ndargmax,
+    normal_vector_field,
+    split_nd,
+)
