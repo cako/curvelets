9247aaccb3ede5551674c0faba2f70f5cc2dbb3c
Carlos da Costa <cako@users.noreply.github.com>
2024-11-23 22:40:59 -0800

    fix: copydoc

--- docs/conf.py
+++ docs/conf.py
@@ -51,3 +51,5 @@ sphinx_gallery_conf = {
     "examples_dirs": "../examples",  # path to your example scripts
     "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
 }
+
+copybutton_prompt_text = ">>> "
