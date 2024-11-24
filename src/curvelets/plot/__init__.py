f0981e253a0994e068d569e3372080c215b6bc00
Carlos da Costa <cako@users.noreply.github.com>
2024-11-23 22:32:41 -0800

    fix: add new matplotlib funcs

--- src/curvelets/plot/__init__.py
+++ src/curvelets/plot/__init__.py
@@ -1,4 +1,25 @@
 from __future__ import annotations
 
-__all__ = ["create_colorbar", "despine"]
-from ._matplotlib import create_colorbar, despine
+__all__ = [
+    "create_colorbar",
+    "create_inset_axes_grid",
+    "despine",
+    "overlay_arrows",
+    "overlay_disk",
+]
+import logging
+
+from .._internal import MATPLOTLIB_ENABLED
+from ._curvelet import overlay_disk
+
+logger = logging.getLogger()
+
+if MATPLOTLIB_ENABLED:
+    from ._matplotlib import (
+        create_colorbar,
+        create_inset_axes_grid,
+        despine,
+        overlay_arrows,
+    )
+else:
+    logger.warning("matplotlib is not installed, not all functions will be available")
