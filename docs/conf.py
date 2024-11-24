a8cc11917ec0fad61b1890b00911c63bfe1804c8
Carlos da Costa <cako@users.noreply.github.com>
2024-11-23 22:31:03 -0800

    fix: add intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ myst_enable_extensions = [
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "matplotlib": ("https://matplotlib.org/", None),
 }
 
 nitpick_ignore = [
