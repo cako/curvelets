cc159fb5083338757a274cb2b8ccd0f610b1912b
Carlos da Costa <cako@users.noreply.github.com>
2025-02-23 19:18:27 -0800

    feature: ucurv added verbatim (only formatting changed)

---
+++ src/curvelets/ucurv/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from .meyerwavelet import *
+from .ucurv import *
+from .util import *
+
+
+# Code imported from https://github.com/yud08/ucurv/tree/82d71a4689a970ac1a21fecbccb23269cac5e6ba
