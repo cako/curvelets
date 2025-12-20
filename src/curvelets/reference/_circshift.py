02c9437dac729c7b8528f9b9f58f40ea4d142413
Carlos da Costa <cako@users.noreply.github.com>
2025-12-20 02:29:55 -0800

    refactor: remove deprecated curvelet reference files and associated modules to streamline the codebase and improve maintainability

--- src/curvelets/reference/_circshift.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from numpy import roll
-
-
-def circshift(arr, shape):
-    assert arr.ndim == len(shape)
-    return roll(arr, shape, axis=tuple(range(len(shape))))
--- src/curvelets/ucurv/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-from .udct import UDCT
-# Code imported from https://github.com/yud08/ucurv/tree/82d71a4689a970ac1a21fecbccb23269cac5e6ba
