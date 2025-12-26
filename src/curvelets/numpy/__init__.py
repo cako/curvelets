3f353daf7780571a88da27be2ae145f580f50801
Carlos da Costa <cako@users.noreply.github.com>
2025-12-25 19:08:31 -0800

    refactor(numpy): remove unused ParamUDCT import from __init__.py
    - Eliminated the import of ParamUDCT from the numpy module's __init__.py file to streamline the code and improve clarity.
    - Updated the __all__ list to reflect the removal of the unused import, ensuring consistency in the module's public API.

--- src/curvelets/numpy/__init__.py
+++ src/curvelets/numpy/__init__.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-__all__ = ["UDCT", "MeyerWavelet", "ParamUDCT", "UDCTCoefficients", "UDCTWindows"]
+__all__ = ["UDCT", "MeyerWavelet", "UDCTCoefficients", "UDCTWindows"]
 
 from ._meyerwavelet import MeyerWavelet
 from ._typing import UDCTCoefficients, UDCTWindows
 from ._udct import UDCT
-from ._utils import ParamUDCT
